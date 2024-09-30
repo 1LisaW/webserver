4026971d85135b78f40e035d0bc420cb18e613c9
root <root@LAPTOP-4954DC8J>
2024-09-30 14:13:48 +0300

    feat: template webserv class

---
+++ Webserver.hpp
@@ -0,0 +1,25 @@
+#ifndef WEBSERVER_H
+# define WEBSERVER_H
+
+class Webserver
+{
+private:
+public:
+	Webserver();
+	~Webserver();
+	int start();
+};
+
+Webserver::Webserver()
+{
+}
+
+Webserver::~Webserver()
+{
+}
+int Webserver::start(){
+
+}
+
+
+#endif
